cc563a3d0d33be2e346d9684840a3636bfcb9199
Hack Reactor Students <github@hackreactor.com>
2013-10-16 17:04:18 -0700

    Downloading sites from sites.txt

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
+var httpGet = require('http-get');
 
 
 exports.readUrls = function(filePath, cb){
-  console.log(filePath);
   var siteStr;
   fs.readFile(filePath,{encoding: 'utf8'}, function(err, data) {
     if(err) throw err;
@@ -12,4 +12,17 @@ exports.readUrls = function(filePath, cb){
 
 exports.downloadUrls = function(urls){
   urls = urls.split("\n");
+
+
+  for(var i = 0 ; i < urls.length; i++){
+    var options = {url : urls[i]};
+    httpGet.get(options, '../data/sites/'+urls[i], function (error, result) {
+      if (error) {
+        console.log(result);
+        console.error(error);
+      } else {
+        console.log('File downloaded at: ' + result.file);
+      }
+    });
+  }
 };
